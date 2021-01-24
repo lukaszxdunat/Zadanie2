59bad0e097bd966cd491dbe427a50034708efe0c
Łukasz Dunat <lukaszxdunat@gmail.com>
2021-01-24 15:08:02 +0100

    initial-commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NewTable from './components/NewTable';
 
 function App() {
+  useEffect(() => {
+    document.title = "Helion - Zadanie Nr 2";
+  }, [])
+
   return (
    <NewTable></NewTable>
   );
